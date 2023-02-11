da72d170ee95e52a4a84ab63f8f5c150066ee325
MD Jahid <deadme094@gmail.com>
2023-02-11 18:45:40 +0600

    did lil stuffs with variables

---
+++ variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+  int a = 50;
+  int b = 10;
+  int c;
+  c = 500;
+
+  printf("%d\n", c);
+  printf("%d + %d = %d\n", a, b, a+b);
+  printf("%d - %d = %d\n", a, b, a-b);
+  printf("%d * %d = %d\n", a, b, a*b);
+  printf("%d / %d = %d", a, b, a/b);
+
+  return 0;
+}
